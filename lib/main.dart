f51b7fb74dbe625b9124eceedf0ffd9a318f01d8
banghid <wahidrochman@gmail.com>
2022-03-16 22:15:27 +0700

    adding new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'dart:ffi';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'style.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: GetStarted(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: GetStarted());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: GetStarted());
   }
 }
 
@@ -95,47 +93,102 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.fromLTRB(50, 30, 50, 30),
-        child: CustomScrollView(
-          slivers: [
-            Container(
-              margin: EdgeInsets.only(bottom: 48),
-              child: Image.asset('assets/images/login_illust.png'),
-            ),
-            Container(
-                margin: EdgeInsets.only(bottom: 9),
-                child: Text(
-                  'First, you need login or create account',
-                  style: GoogleFonts.poppins(
-                      fontSize: 14, fontWeight: FontWeight.bold),
-                )),
-            Container(
-              margin: EdgeInsets.only(top: 9, bottom: 9),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    hintText: 'Your email ex: youremail@domain.com'),
+        margin: EdgeInsets.fromLTRB(50, 70, 50, 70),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 48),
+                child: Image.asset('assets/images/login_illust.png'),
               ),
-            ),
-            Container(
-              child: Row(
-                children: [
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          minimumSize: Size(148, 45),
-                          maximumSize: Size(200, 45),
-                          primary: Style.cancelColor),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Cancel',
+              Container(
+                  margin: EdgeInsets.only(bottom: 9),
+                  child: Text(
+                    'First, you need login or create account',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14, fontWeight: FontWeight.bold),
+                  )),
+              Container(
+                margin: EdgeInsets.only(top: 9, bottom: 9),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 5.0)),
+                      hintText: 'Your email ex: youremail@domain.com',
+                      hintStyle: GoogleFonts.poppins(
+                          fontSize: 12, color: Style.hintColor)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 9, bottom: 10),
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      hintText: 'Your password here :)',
+                      hintStyle: GoogleFonts.poppins(
+                          fontSize: 12, color: Style.hintColor)),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  alignment: Alignment.topLeft,
+                  child: RichText(
+                      text: TextSpan(
+                          text: 'Forgot your password?',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text('Tapped forgot password')));
+                            },
                           style: GoogleFonts.poppins(
-                              fontSize: 14, fontWeight: FontWeight.bold))),
-                ],
-              ),
-            )
-          ],
+                              fontSize: 12,
+                              decoration: TextDecoration.underline,
+                              color: Colors.black)))),
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 6),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(148, 45),
+                              maximumSize: Size(200, 45),
+                              primary: Style.cancelColor),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 6),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(148, 45),
+                              maximumSize: Size(200, 45),
+                              primary: Style.accentColor),
+                          onPressed: () {},
+                          child: Text('Login',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white))),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
